c7078e5cd30d62464aace5c758c5a32ca922e40a
Alex <alexfrancisaaron@gmail.com>
2021-07-06 12:16:48 -0500

    tested conditional funcitonality... failed

--- src/challenges.js
+++ src/challenges.js
@@ -3,8 +3,13 @@ let arr = [0, 0, 1, 0, 0, 1, 0];
 function jumpingOnClouds(c) {
     let jumps = 0;
     for (let i = 0; i < c.length; i++) {
-        if (c[i + 1] === 0) {
+        if (c[i + 2] === 0) {
             jumps += 1;
+            console.log(jumps);
+        }
+        else if (c[i + 1] === 0) {
+            jumps += 1;
+            console.log(jumps);
         }
     }
     return jumps;
