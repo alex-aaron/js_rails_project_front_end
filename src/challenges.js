97c7231ed6c409027dfc86a2e21c23da19daa52e
Alex <alexfrancisaaron@gmail.com>
2021-07-07 14:04:09 -0500

    revised hourglass function

--- src/challenges.js
+++ src/challenges.js
@@ -9,6 +9,23 @@ let sampleArr = [
 
 function hourglassSum(arr) {
     let sumArr = [];
+    let largestSum = 0;
+    let hourglassIndices = [0, 1, 2, 3, 
+                            6, 7, 8, 9,
+                            12, 13, 14, 15,
+                            18, 19, 20, 21];
+    for (let i = 0; i < arr.length; i++) {
+        if (hourglassIndices.includes(i)) {
+            let sum = arr[i] + (arr[i + 1]) + (arr[i + 2]) + (arr[i + 7]) + (arr[i + 12]) + (arr[i + 13]) + (arr[i + 14]);
+            sumArr.push(sum);
+        }
+    }
+    for (let i = 0; i < sumArr.length; i++) {
+        if (sumArr[i] >= largestSum) {
+            largestSum = sumArr[i];
+        }
+    }
+    return largestSum;
 }
 
 let output = hourglassSum(sampleArr);
