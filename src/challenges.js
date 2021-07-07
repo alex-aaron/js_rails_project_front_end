de70bac297b53a41f4bfaa6408861a8c82d41844
Alex <alexfrancisaaron@gmail.com>
2021-07-07 10:18:39 -0500

    created hourglasssum function with sample input

--- src/challenges.js
+++ src/challenges.js
@@ -1,29 +1,15 @@
-let num = 9;
-let sampleArr = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+let sampleArr = [
+    -9, -9, -9, 1, 1, 1,
+     0, -9, 0,  4, 3, 2,
+    -9, -9, -9, 1, 2, 3,
+     0,  0,  8, 6, 6, 0,
+     0,  0,  0, -2, 0, 0,
+     0,  0,  1,  2, 4, 0
+]
 
-function sockMerchant(n, ar) {
-    let sockObj = {};
-    let numPairs = 0;
-    for (const element of ar) {
-        if (sockObj[element] === undefined) {
-            sockObj[element] = 1;
-        }
-        else {
-            sockObj[element] += 1;
-        }
-    }
-    for (let key in sockObj) {
-        if (sockObj[key] % 2 === 0) {
-            let indPairs = sockObj[key] / 2;
-            numPairs += indPairs;
-        }
-        else {
-            indPairs = Math.floor(sockObj[key] / 2);
-            numPairs += indPairs;
-        }
-    }
-    return numPairs;
+function hourglassSum(arr) {
+    let sumArr = [];
 }
 
-let output = sockMerchant(num, sampleArr);
+let output = hourglassSum(sampleArr);
 console.log(output);
