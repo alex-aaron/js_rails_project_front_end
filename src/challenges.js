8bce70c83167c91b0d141908f2836389317e7b46
Alex <alexfrancisaaron@gmail.com>
2021-07-08 15:50:35 -0500

    solved sevenBoom edabit function

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,13 @@
-let sampleNum = 6;
+let sampleArr = [1, 2, 3, 5, 6, 7];
 
-const determineFactorial = (num) => {
-    let nextNum = 
+const sevenBoom = arr => {
+    if (arr.includes(7)) {
+        console.log("Boom!");
+    }
+    else {
+        console.log("there is no 7 in the array");
+    }
+
 }
+
+sevenBoom(sampleArr);
