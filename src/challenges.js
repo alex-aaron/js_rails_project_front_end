6063f86b988d92c4d7f91b41ee9ba2993bb37048
Alex <alexfrancisaaron@gmail.com>
2021-07-08 16:05:27 -0500

    removed sevenBoom function

--- src/challenges.js
+++ src/challenges.js
@@ -1,13 +1,3 @@
 let sampleArr = [1, 2, 3, 5, 6, 7];
 
-const sevenBoom = arr => {
-    if (arr.includes(7)) {
-        console.log("Boom!");
-    }
-    else {
-        console.log("there is no 7 in the array");
-    }
-
-}
-
 sevenBoom(sampleArr);
